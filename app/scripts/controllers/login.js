f3a94f0f4fc98d85fd40d992f469bb3a50ddf115
Dan Lourenco <daniel.lourenco@fmr.com>
2014-12-10 19:31:39 -0500

    add auth service

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,6 +1,3 @@
-/**
- * Created by dloure on 12/5/14.
- */
 'use strict';
 
 /**
@@ -10,12 +7,45 @@
  * # MainCtrl
  * Controller of the durbrowPerformanceAngularApp
  */
+/*
 app.controller('LoginCtrl',
-  function ($scope) {
+  function ($scope, $firebase) {
+
+
+    var sync = Auth;
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
     $scope.page = 'Login Page';
+
+    // replace node with new data.
+    sync.$set({foo: 'bazz'});
+
+    sync.$push({
+      hello: 'world'
+    })
+    .then(function(newChildRef) {
+      console.log("added record with id: " + newChildRef.key());
+    });
+
+    sync.$remove('foo');
+
+    var changedData = {foo: 'bar', bar: {hello: 'world'}, baz: null};
+    sync.$update(changedData);
+
+
   });
+*/
+
+
+
+
+// and use it in our controller
+app.controller("LoginCtrl", ["$scope", "Auth", function($scope, Auth) {
+  $scope.auth = Auth;
+  $scope.user = $scope.auth.$getAuth();
+}]);
+
---
+++ app/scripts/services/auth.js
@@ -0,0 +1,7 @@
+/**
+ * Created by a545703 on 12/10/14.
+ */
+app.factory('Auth', ["$firebaseAuth", function($firebaseAuth){
+  var ref = new Firebase('https://durbrow-performance.firebaseio.com');
+  return $firebaseAuth(ref);
+}]);
