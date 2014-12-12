61999dbd11b16ae2e83fbd068c5f5f932e2f2425
Laura Vecchio <laura.m.vecchio@gmail.com>
2014-12-12 11:48:36 -0500

    Converted script files from CoffeeScript to JavaScript

--- app/scripts/services/auth.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-/**
- * Created by a545703 on 12/10/14.
- */
-app.factory('Auth', ['$firebaseAuth', function($firebaseAuth){
-  var ref = new Firebase('https://durbrow-performance.firebaseio.com');
-  return $firebaseAuth(ref);
-}]);
