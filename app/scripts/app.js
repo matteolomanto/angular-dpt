a9aeca946bf903d03a59150763657919c2332d3d
Laura Vecchio <laura.m.vecchio@gmail.com>
2014-12-31 12:10:08 -0500

    Removing unnecessary routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,63 +20,16 @@
     'app.task',
     'app.measurements.ctrls',
     'app.exercises.ctrls',
-    'app.page.ctrls'
-  ]).config([
+    'app.page.ctrls',
+    'firebase'
+  ])
+    .constant('FIREBASE_URL', 'https://durbrow-performance.firebaseio.com/')
+    .config([
     '$routeProvider', function($routeProvider) {
       return $routeProvider.when('/', {
         redirectTo: '/pages/signin'
       }).when('/dashboard', {
         templateUrl: 'views/dashboard.html'
-      }).when('/ui/typography', {
-        templateUrl: 'views/ui/typography.html'
-      }).when('/ui/buttons', {
-        templateUrl: 'views/ui/buttons.html'
-      }).when('/ui/icons', {
-        templateUrl: 'views/ui/icons.html'
-      }).when('/ui/grids', {
-        templateUrl: 'views/ui/grids.html'
-      }).when('/ui/widgets', {
-        templateUrl: 'views/ui/widgets.html'
-      }).when('/ui/components', {
-        templateUrl: 'views/ui/components.html'
-      }).when('/ui/timeline', {
-        templateUrl: 'views/ui/timeline.html'
-      }).when('/ui/nested-lists', {
-        templateUrl: 'views/ui/nested-lists.html'
-      }).when('/ui/pricing-tables', {
-        templateUrl: 'views/ui/pricing-tables.html'
-      }).when('/forms/elements', {
-        templateUrl: 'views/forms/elements.html'
-      }).when('/forms/layouts', {
-        templateUrl: 'views/forms/layouts.html'
-      }).when('/forms/validation', {
-        templateUrl: 'views/forms/validation.html'
-      }).when('/forms/wizard', {
-        templateUrl: 'views/forms/wizard.html'
-      }).when('/maps/gmap', {
-        templateUrl: 'views/maps/gmap.html'
-      }).when('/maps/jqvmap', {
-        templateUrl: 'views/maps/jqvmap.html'
-      }).when('/tables/static', {
-        templateUrl: 'views/tables/static.html'
-      }).when('/tables/responsive', {
-        templateUrl: 'views/tables/responsive.html'
-      }).when('/tables/dynamic', {
-        templateUrl: 'views/tables/dynamic.html'
-      }).when('/charts/others', {
-        templateUrl: 'views/charts/charts.html'
-      }).when('/charts/morris', {
-        templateUrl: 'views/charts/morris.html'
-      }).when('/charts/flot', {
-        templateUrl: 'views/charts/flot.html'
-      }).when('/mail/inbox', {
-        templateUrl: 'views/mail/inbox.html'
-      }).when('/mail/compose', {
-        templateUrl: 'views/mail/compose.html'
-      }).when('/mail/single', {
-        templateUrl: 'views/mail/single.html'
-      }).when('/pages/features', {
-        templateUrl: 'views/pages/features.html'
       }).when('/pages/measurements', {
         templateUrl: 'views/pages/measurements.html'
       }).when('/pages/signin', {
@@ -95,14 +48,6 @@
         templateUrl: 'views/pages/500.html'
       }).when('/pages/blank', {
         templateUrl: 'views/pages/blank.html'
-      }).when('/pages/invoice', {
-        templateUrl: 'views/pages/invoice.html'
-      }).when('/pages/services', {
-        templateUrl: 'views/pages/services.html'
-      }).when('/pages/about', {
-        templateUrl: 'views/pages/about.html'
-      }).when('/pages/contact', {
-        templateUrl: 'views/pages/contact.html'
       }).when('/goals', {
         templateUrl: 'views/goals/goals.html'
       }).otherwise({
